cddf272c79038ddacc0d287959ecdc093e5b0597
mobluse <mikael.bonnier@gmail.com>
2016-01-12 13:19:45 +0100

    Added sh-files.

---
+++ rebuild.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+gcc blockis.c -o blockis -lncurses -std=c99
---
+++ scale-font.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Scale font to let Blockis fill screen.
+# These settings work for composite video (PAL):
+#CHARMAP="UTF-8"
+#CODESET="Lat15"
+#FONTFACE="Terminus"
+#FONTSIZE="10x20"
+# Edit /etc/default/console-setup or enter in:
+sudo dpkg-reconfigure console-setup
