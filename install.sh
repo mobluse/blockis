fc0d68629da56c846fbabece576aa1798d358cd5
mobluse <mikael.bonnier@gmail.com>
2016-01-20 14:32:07 +0100

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt-get install libncurses5-dev
+echo "How to switch on SPI:"
+echo "sudo raspi-config"
+echo "9 Advanced Options"
+echo "A6 SPI"
+echo "Yes"
+echo "Yes"
+echo "Finish"
+echo "Yes"
